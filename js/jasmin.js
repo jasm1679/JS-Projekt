f60d20a7579653117fdb10a09fc7bb5ee2de3d95
jasm1679 <jdch48625@edu.ucl.dk>
2023-02-21 17:46:36 +0100

    Finpudsning af header

--- js/jasmin.js
+++ js/jasmin.js
@@ -2,7 +2,7 @@
         const mellemRum = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
 
     // Array
-        const linje  = ["Trustpilot 4,3", "Levering 1-3 arbejdsdage", "Fri fragt på køb over 649 dkk"];
+        const linje  = ["✔ Trustpilot 4,3", "✔ Levering 1-3 arbejdsdage", "✔ Fri fragt på køb over 649 dkk"];
         let output = document.getElementById("out");
 
     // for-loop
