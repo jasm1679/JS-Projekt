d348c38bd8b0415e8821c0d002d64f2d2df96183
jasm1679 <jdch48625@edu.ucl.dk>
2023-02-21 20:28:10 +0100

    Knapper
    Nogle billeder er lavet om til knapper

--- js/jasmin.js
+++ js/jasmin.js
@@ -9,3 +9,33 @@
         for (let i = 0; i < linje.length; i++) {
             output.innerHTML += mellemRum + linje[i]
     }
+
+
+
+    
+
+
+
+
+// Antal
+    function plus() {
+        let value, inputObject;
+      
+        inputObject = document.getElementById("antal");
+        value=parseFloat(inputObject.value);
+        if (isNaN(value)) {
+        } else {
+          inputObject.value=value+1;
+        }
+      }
+
+      function minus() {
+        let value, inputObject;
+      
+        inputObject = document.getElementById("antal");
+        value=parseFloat(inputObject.value);
+        if (isNaN(value)) {
+        } else {
+          inputObject.value=value-1;
+        }
+      }
