b195de38bbd86fd553324a741b16acba16a1b56c
BenjaHQ <benjamin-phan@hotmail.com>
2023-02-22 10:33:25 +0100

    Slideshow fixed

---
+++ js/benjamin.js
@@ -0,0 +1,14 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("slideimages");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); 
+}
