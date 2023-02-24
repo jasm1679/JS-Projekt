f96ea92bf6c3eea1461955704b0775842a7302ca
JulietDane <julieringivehansen@outlook.com>
2023-02-24 16:32:28 +0100

    Update julie.js

---
+++ js/julie.js
@@ -0,0 +1,41 @@
+//Dette er hvad jeg har prøvet at lave. Er meget usikker på om det virker eller ej,
+//Jeg ved at der er fejl. Det har været en for stor opgave for mig at lave.
+//Jeg har prøvet på at få counteren til at gå op og ned med + og - tasterne.
+
+const antal = document.getElementById("antal");
+
+let count = 0;
+
+antal.innerHTML = count;
+
+//incress
+const handleIncrement = () => {
+    count ++;
+    antal.innerHTML = count;
+};
+
+//decress
+const handleDecrement = () => {
+    count-- ;
+    antal.innerHTML = count;
+}
+
+//button
+const incrementCount = document.getElementById("increment-count");
+const decrementCount = document.getElementById("decrement-count");
+
+incrementCount.addEventListner("click", handleIncrement);
+decrementCount.addEventListner("click", handleDecrement);
+
+const numbers = ["one", "five", "ten", "twenty", "fifty", "onehundred"];
+
+for (i = 0; i < x.length; i++) {
+    x[i].onclick = function() {
+    if(antal > 0) {
+        incrementCount == "bi bi-cart4" ;
+    }
+    if(antal <= 0) {
+        decrementCount == "bi bi-cart4";
+    }
+}}
+
