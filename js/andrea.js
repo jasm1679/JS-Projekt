24f50376cc36cf294f815b831b0f93952363c36e
Andreaandkjaer <avva48635@edu.ucl.dk>
2023-02-25 15:40:45 +0100

    a-p2

--- js/andrea.js
+++ js/andrea.js
@@ -1,15 +1,4 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("visual");
+function getOption() {
+  selectElement = document.querySelector("#button");
+  output = selectElement.value;
 }
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbutton")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("visual")) {
-        openDropdown.classList.remove("visual");
-      }
-    }
-  }
-};
