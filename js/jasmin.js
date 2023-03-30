49edf37b64399acfc07f3bb8467d9b6cabe9f9dd
jasm1679 <jdch48625@edu.ucl.dk>
2023-03-30 10:13:29 +0200

    Update jasmin.js

--- js/jasmin.js
+++ js/jasmin.js
@@ -1,3 +1,15 @@
+// Bjælke
+        const mellemRum = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+
+    // Array
+        const linje  = ["✔ Trustpilot 4,3", "✔ Levering 1-3 arbejdsdage", "✔ Fri fragt på køb over 649 dkk"];
+        let output = document.getElementById("out");
+
+    // for-loop
+        for (let i = 0; i < linje.length; i++) {
+            output.innerHTML += mellemRum + linje[i]
+    }
+    
 // Bil
     const scrollArea = 1700 - innerHeight;
     let koretoj;
@@ -26,17 +38,7 @@
       }
     }
 
-// Bjælke
-        const mellemRum = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
 
-    // Array
-        const linje  = ["✔ Trustpilot 4,3", "✔ Levering 1-3 arbejdsdage", "✔ Fri fragt på køb over 649 dkk"];
-        let output = document.getElementById("out");
-
-    // for-loop
-        for (let i = 0; i < linje.length; i++) {
-            output.innerHTML += mellemRum + linje[i]
-    }
 
 
 
